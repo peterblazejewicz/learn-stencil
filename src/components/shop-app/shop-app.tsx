988e1d47c61689eedde12120196e044ea3c5de4d
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-08-22 00:23:10 +0200

    First component

---
+++ src/components/shop-app/shop-app.tsx
@@ -0,0 +1,17 @@
+import { Component } from '@stencil/core';
+
+
+@Component({
+  tag: 'shop-app',
+  styleUrl: 'shop-app.scss'
+})
+export class ShopApp {
+
+  render() {
+    return (
+      <p>
+        SHOPA
+      </p>
+    );
+  }
+}
--- stencil.config.js
+++ stencil.config.js
@@ -1,6 +1,6 @@
 exports.config = {
   bundles: [
-    { components: ['my-name'] }
+    { components: ['shop-app'] }
   ],
   collections: [
     { name: '@stencil/router' }
